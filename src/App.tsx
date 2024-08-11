9b4d81fa4f48af84916dcf8038d5c3ffc61f869a
Ying Zhou <ybzhou@thoughtworks.com>
2024-08-11 21:18:13 +0800

    add router, stub homepage and stub movie detail page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
 import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
+import MovieDetail from "./pages/MovieDetail";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home></Home>,
+  },
+  {
+    path: "/home",
+    element: <Home></Home>,
+  },
+  {
+    path: "/movies/:id",
+    element: <MovieDetail></MovieDetail>,
+  },
+]);
 
 function App() {
-  return (
-    <>
-      <h1>Movie Rating</h1>
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/pages/MovieDetail/index.tsx
@@ -0,0 +1,8 @@
+export default function MovieDetail() {
+  return (
+    <div>
+      <h1>Movie Detail</h1>
+      <p>Welcome to the movie detail page!</p>
+    </div>
+  );
+}
