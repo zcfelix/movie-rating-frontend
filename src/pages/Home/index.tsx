63e13de0e6625083a67d21e40ee5ee48cdbadc48
Ying Zhou <ybzhou@thoughtworks.com>
2024-08-11 22:37:58 +0800

    implement simple SearchBar

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,23 @@
-import { Button } from "../../components/ui/button";
+import { useState } from "react";
+import { SearchBar } from "./components/SearchBar";
 
 export default function Home() {
+  // create state for search value
+  const [searchValue, setSearchValue] = useState("");
+
+  const handleSearchClick = () => {
+    console.log("search value: ", searchValue);
+  };
+
+  const handleSearchValueChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchValue(e.target.value);
+  }
+
   return (
     <div>
       <h1 className='text-3xl font-bold underline'>Home</h1>
       <p>Welcome to the home page!</p>
-      <Button>Cool</Button>
+      <SearchBar value={searchValue} onValueChange={handleSearchValueChange} onSearchClick={handleSearchClick} />
     </div>
   );
 }
