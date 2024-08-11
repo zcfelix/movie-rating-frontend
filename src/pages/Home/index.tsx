b2f44d2efa922e9448295a4db7caf3e7d7ea3853
Ying Zhou <ybzhou@thoughtworks.com>
2024-08-11 22:04:14 +0800

    test button component

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,11 @@
+import { Button } from "../../components/ui/button";
+
 export default function Home() {
   return (
     <div>
       <h1 className='text-3xl font-bold underline'>Home</h1>
       <p>Welcome to the home page!</p>
+      <Button>Cool</Button>
     </div>
   );
 }
