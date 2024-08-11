fa4f4c9bfafa7d3476e220c6376b1a4364a210a3
Ying Zhou <ybzhou@thoughtworks.com>
2024-08-11 21:58:35 +0800

    add tailwind and shadcn support

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <h1>Home</h1>
+      <h1 className='text-3xl font-bold underline'>Home</h1>
       <p>Welcome to the home page!</p>
     </div>
   );
