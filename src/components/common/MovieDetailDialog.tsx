6e5f7c5e44ee860f50b6d8377e55d78c54fec6aa
Ying Zhou <ybzhou@thoughtworks.com>
2024-08-15 16:29:30 +0800

    refactor: MovieDetailDialog

--- src/components/common/MovieDetailDialog.tsx
+++ src/components/common/MovieDetailDialog.tsx
@@ -7,8 +7,11 @@ import {
   DialogTrigger,
 } from '../ui/dialog.tsx';
 import { Movie } from '../../types/movie.ts';
-import RatingDialog from './RatingDialog.tsx';
 import MovieImg from './MovieImg.tsx';
+import ReleaseDate from '../../pages/Movies/components/ReleaseDate.tsx';
+import Runtime from '../../pages/Movies/components/Runtime.tsx';
+import Rating from '../../pages/Movies/components/Rating.tsx';
+import RatingDialog from './RatingDialog.tsx';
 
 const MovieDetailDialog = ({
   children,
@@ -25,22 +28,28 @@ const MovieDetailDialog = ({
           <DialogTitle className="flex gap-4">
             <MovieImg url={movie.posterUrl} className="w-[80px] h-[120px]" />
             <div>
-              <div>{movie.title}</div>
-              <div className="flex">
-                <div className="m-1">{movie.averageRating}</div>
+              <div className="text-lg font-bold">{movie.title}</div>
+              <div className="flex flex-row space-x-4 mt-0.5">
+                <ReleaseDate releaseYear={movie.year} />
+                <Runtime runtime={movie.runtime} />
+              </div>
+              <div className="flex flex-row gap-4">
+                <Rating rating={movie.averageRating} />
                 <RatingDialog movie={movie} />
               </div>
             </div>
           </DialogTitle>
           <DialogDescription>
-            <span className="font-bold">Director: </span>
+            <span className="font-bold text-black mr-1">Director</span>
             <span className="text-blue-500">{movie.director}</span>
           </DialogDescription>
           <DialogDescription>
-            <span className="font-bold">Actors: </span>
+            <span className="font-bold text-black mr-1">Actors</span>
             <span className="text-blue-500">{movie.actors}</span>
           </DialogDescription>
-          <DialogDescription>{movie.plot}</DialogDescription>
+          <DialogDescription className="text-black">
+            {movie.plot}
+          </DialogDescription>
         </DialogHeader>
       </DialogContent>
     </Dialog>
