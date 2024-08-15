c836ff48e643ddb95139cffa400703f1fbeb1e14
Ying Zhou <ybzhou@thoughtworks.com>
2024-08-15 15:33:51 +0800

    feat: add display info in MovieList component

--- src/pages/Home/components/MovieList.tsx
+++ src/pages/Home/components/MovieList.tsx
@@ -9,20 +9,37 @@ import MovieDetailDialog from '../../../components/common/MovieDetailDialog.tsx'
 import MovieContext from '../../../context/movies';
 import { useContext } from 'react';
 import MovieImg from '../../../components/common/MovieImg.tsx';
+import ReleaseDate from '../../Movies/components/ReleaseDate.tsx';
+import Runtime from '../../Movies/components/Runtime.tsx';
+import Rating from '../../Movies/components/Rating.tsx';
 
 const MovieList = () => {
   const { movies } = useContext(MovieContext);
   return (
     <Table className="my-6">
       <TableBody>
-        {movies.map((movie) => (
+        {movies.map((movie, index) => (
           <TableRow key={movie.id}>
             <TableCell className="font-medium w-[100px]">
               <MovieImg url={movie.posterUrl} className="w-[100px]" />
             </TableCell>
             <TableCell className="flex flex-col text-left">
-              <div className="text-lg font-bold">{movie.title}</div>
-              <div>{movie.averageRating}</div>
+              <div className="text-lg font-bold">
+                {`${index + 1}. ${movie.title}`}
+              </div>
+              <div className="flex flex-row space-x-4 mt-0.5">
+                <ReleaseDate releaseYear={movie.year} />
+                <Runtime runtime={movie.runtime} />
+                <Rating rating={movie.averageRating} />
+              </div>
+              <p className="mt-0.5">
+                <span className="font-bold text-sm mr-2">Director</span>
+                <span className="text-blue-500">{movie.director}</span>
+              </p>
+              <p className="mt-0.5">
+                <span className="font-bold text-sm mr-2">Actors</span>
+                <span className="text-blue-500">{movie.actors}</span>
+              </p>
             </TableCell>
             <TableCell className="text-right w-[100px]">
               <MovieDetailDialog movie={movie}>
--- src/pages/Movies/components/Rating.tsx
+++ src/pages/Movies/components/Rating.tsx
@@ -2,12 +2,10 @@ import { Star } from 'lucide-react';
 
 const Rating = ({ rating }: { rating: string }) => {
   return (
-    <>
-      <div className="flex justify-center items-center space-x-1">
-        <Star className="w-4 h-4 text-yellow-400 fill-current" />
-        <span className="text-sm text-gray-500">{rating}</span>
-      </div>
-    </>
+    <div className="flex justify-center items-center space-x-1">
+      <Star className="w-4 h-4 text-yellow-400 fill-current" />
+      <span className="text-sm text-gray-500">{rating}</span>
+    </div>
   );
 };
 
